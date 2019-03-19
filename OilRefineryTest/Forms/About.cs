7e91a67f14f3c660535075b0eef239bd20b68dd4
choppingkid <chestofak@yandex.ru>
2019-03-19 15:47:50 +0300

    ONE MORE TEST

--- OilRefineryTest/Forms/About.cs
+++ OilRefineryTest/Forms/About.cs
@@ -19,7 +19,7 @@ namespace OilRefineryTest.Forms
 
         private void button1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("TI PIDOR");
+            MessageBox.Show("TI PIDORRRRRRRRRRRR");
         }
     }
 }
